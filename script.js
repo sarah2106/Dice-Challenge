d6dea3bf35409ebb7436f6dd57c888ab54bba1f8
gebruikersarah2106 <nosaaartje@yahoo.com>
2020-05-14 12:48:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    const RandomNumber1 = Math.floor(Math.random() * 6) + 1;
+    const RandomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+    const randomDiceImage = 'dice' + RandomNumber1 + '.png';
+    const randomImageSource = 'images/' + randomDiceImage ;
+    
+    
